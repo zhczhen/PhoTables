6fa1381ca26182e0b05d52877654bb18079d9162
collinization <collinzizhen@gmail.com>
2021-07-23 21:41:10 -0700

    Spaces are properly rearranged to accomodate the entrance. Now instant gameover if clicked wrong.

--- app.js
+++ app.js
@@ -1,133 +1,128 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
+  const cardArray = [
+    {
+      name: "1",
+      img: "images/1.png",
+    },
+    {
+      name: "2",
+      img: "images/2.png",
+    },
+    {
+      name: "3",
+      img: "images/3.png",
+    },
+    {
+      name: "4",
+      img: "images/4.png",
+    },
+    {
+      name: "5",
+      img: "images/5.png",
+    },
+    {
+      name: "6",
+      img: "images/6.png",
+    },
+    {
+      name: "7",
+      img: "images/7.png",
+    },
+    {
+      name: "8",
+      img: "images/8.png",
+    },
+    {
+      name: "9",
+      img: "images/9.png",
+    },
+    {
+      name: "10",
+      img: "images/10.png",
+    },
+    {
+      name: "11",
+      img: "images/11.png",
+    },
+    {
+      name: "12",
+      img: "images/12.png",
+    },
+    {
+      name: "13",
+      img: "images/13.png",
+    },
+    {
+      name: "14",
+      img: "images/14.png",
+    },
+    {
+      name: "15",
+      img: "images/15.png",
+    },
+    {
+      name: "16",
+      img: "images/16.png",
+    },
+    {
+      name: "17",
+      img: "images/17.png",
+    },
+    {
+      name: "18",
+      img: "images/18.png",
+    },
+    {
+      name: "19",
+      img: "images/19.png",
+    },
+  ];
+  const squares = document.querySelectorAll(".square");
+  const mole = document.querySelector(".mole");
+  const timeLeft = document.querySelector("#time-left");
+  const score = document.querySelector("#score");
+  const table = document.querySelector("#table");
 
-const cardArray = [
-  {
-    name: '1',
-    img: 'images/1.png'
-  },
-  {
-    name: '2',
-    img: 'images/2.png'
-  },
-  {
-    name: '3',
-    img: 'images/3.png'
-  },
-  {
-    name: '4',
-    img: 'images/4.png'
-  },
-  {
-    name: '5',
-    img: 'images/5.png'
-  },
-  {
-    name: '6',
-    img: 'images/6.png'
-  },
-  {
-    name: '7',
-    img: 'images/7.png'
-  },
-  {
-    name: '8',
-    img: 'images/8.png'
-  },
-  {
-    name: '9',
-    img: 'images/9.png'
-  },
-  {
-    name: '10',
-    img: 'images/10.png'
-  },
-  {
-    name: '11',
-    img: 'images/11.png'
-  },
-  {
-    name: '12',
-    img: 'images/12.png'
-  },
-  {
-    name: '13',
-    img: 'images/13.png'
-  },
-  {
-    name: '14',
-    img: 'images/14.png'
-  },
-  {
-    name: '15',
-    img: 'images/15.png'
-  },
-  {
-    name: '16',
-    img: 'images/16.png'
-  },
-  {
-    name: '17',
-    img: 'images/17.png'
-  },
-  {
-    name: '18',
-    img: 'images/18.png'
-  },
-  {
-    name: '19',
-    img: 'images/19.png'
+  let result = 0;
+  let hitPosition;
+  let currentTime = 60;
+  let timerId = null;
+  let matherino = 0;
+  let cardsChosen = [];
+  let cardsChosenId = [];
+  let randomSquared = null;
+
+  function randomSquare() {
+    squares.forEach((square) => {
+      square.classList.remove("mole");
+    });
+    matherino = Math.floor(Math.random() * 25);
+    console.log("before " + matherino);
+
+    while (matherino == 1 || matherino == 2 || matherino == 3) {
+      matherino = Math.floor(Math.random() * 25);
+      console.log("inside " + matherino);
+    }
+    console.log("after " + matherino);
+    randomSquared = squares[matherino];
+    table.textContent = randomSquared.id;
+
+    squares[1].classList.add("null1");
+    squares[2].classList.add("null2");
+    squares[3].classList.add("null3");
+
+    //randomSquared.classList.add('mole')
+
+    hitPosition = randomSquared.id;
   }
-]
-const squares = document.querySelectorAll('.square')
-const mole = document.querySelector('.mole')
-const timeLeft = document.querySelector('#time-left')
-const score = document.querySelector('#score')
-const table = document.querySelector('#table')
 
-
-let result = 0
-let hitPosition
-let currentTime = 60
-let timerId = null
-let matherino = 0
-let cardsChosen = []
-let cardsChosenId = []
-let randomSquared = null
-
-function randomSquare() {
-  squares.forEach(square => {
-    square.classList.remove('mole')
-  })
-  matherino = Math.floor(Math.random() * 25)
-  console.log("before "+matherino)
-
-  while(matherino==21 || matherino==22 || matherino==23){
-    matherino = Math.floor(Math.random() * 25)
-    console.log("inside "+matherino)
-
-  }
-  console.log("after "+matherino)
-  randomSquared = squares[matherino]
-  table.textContent = randomSquared.id
-
-  squares[21].classList.add('mole')
-  squares[22].classList.add('mole')
-  squares[23].classList.add('mole')
-
-  //randomSquared.classList.add('mole')
-
-
-  hitPosition = randomSquared.id
-}
-
-
-squares.forEach(square => {
-  square.addEventListener('mousedown', () => {
-    if (square.id == hitPosition) {
-      //let cardId = this.getAttribute('data-id')
-      randomSquared.classList.add('mole')
-      //square.classList.add(cardArray[matherino].img)
-      /*cardsChosen.push(cardArray[matherino].name)
+  squares.forEach((square) => {
+    square.addEventListener("mousedown", () => {
+      if (square.id == hitPosition) {
+        //let cardId = this.getAttribute('data-id')
+        randomSquared.classList.add("mole");
+        //square.classList.add(cardArray[matherino].img)
+        /*cardsChosen.push(cardArray[matherino].name)
       cardsChosenId.push(matherino)
       this.setAttribute('src', cardArray[matherino].img)
       if (cardsChosen.length ===2) {
@@ -135,24 +130,29 @@ squares.forEach(square => {
       }
             */
 
-      result++
-      score.textContent = result
-      hitPosition = null
-      setTimeout(randomSquare,1000)
+        result++;
+        score.textContent = result;
+        hitPosition = null;
+        setTimeout(randomSquare, 1000);
+      } else {
+        randomSquared.classList.add("mole");
+        alert("GAME OVER! Your final score is " + result);
+        result = 0;
+        score.textContent = result;
+        hitPosition = null;
+        randomSquare();
+        /*
+        result--;
+        score.textContent = result;
+        hitPosition = null;
+        setTimeout(randomSquare, 1000);
+        */
+      }
+    });
+  });
 
-    }
-    else{
-      randomSquared.classList.add('mole')
-      result--
-      score.textContent = result
-      hitPosition = null
-      setTimeout(randomSquare,1000)
-    }
-  })
-})
-
-randomSquare()
-/*
+  randomSquare();
+  /*
 function moveMole() {
   timerId = setInterval(randomSquare,1000)
 }
@@ -174,4 +174,4 @@ function countDown() {
 
 let countDownTimerId = setInterval(countDown, 1000)
 */
-})
+});
